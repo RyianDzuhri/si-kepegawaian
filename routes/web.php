2fb8d4c5b62de7b8e6fe5aa5cae4a6602ec2898d
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2026-01-26 18:54:52 +0800

    push

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])
     ->name('dashboard');
 
-
 // Rute untuk melihat Manajemen Pegawai
 
 Route::get('/pegawai', [ManajemenPegawaiController::class, 'index'])->name('manajemen-pegawai');
