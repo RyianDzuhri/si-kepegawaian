60637eba8ef76a72890be1958ca0823cd980b8bc
Almunajat-121 <almunajatamirul@gmail.com>
2026-02-09 01:42:32 +0800

    Only BINDO

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator; // <--- Tambahkan ini di atas
+use Illuminate\Pagination\Paginator; 
+use Carbon\Carbon; 
 
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
 {
     Paginator::useBootstrapFive(); // <--- Tambahkan ini di dalam function boot
+    // 2. Tambahkan pengaturan locale Carbon
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
+        date_default_timezone_set('Asia/Jakarta');
+
 }
+
+
 }
